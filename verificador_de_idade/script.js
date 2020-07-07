4a4284e027ddaf4caa4c05aa2d7c93d1fe312d15
vitor-risso <vitor.risso13@gmail.com>
2020-07-07 13:02:32 -0300

    inicio de tudo

---
+++ verificador_de_idade/script.js
@@ -0,0 +1,66 @@
+function verify(){
+
+    var data = new Date()
+    var ano_atual = data.getFullYear() // ano atual para validação de dados
+    var tipo = document.getElementById(`txtano`)
+    var ano = Number(tipo.value)           //conversão string ~ number
+    var res = document.getElementById(`res`)
+
+    console.log( `${ano_atual}`)
+    console.log(' ola' )
+
+    if(ano === 0 || ano > ano_atual){
+        window.alert(`[ERRO]  Verifique os dados e tente novamente`)
+    } else{
+        var fsex = document.getElementsByName(`radsex`) // [1] ou [0] 0 homem 1 mulher; NOe tem q ser igual senão o sistema deixa maracar os dois e não deixa desmaracar
+        let idade = (ano_atual - ano)
+        res.innerHTML = `Idade calculada é de ${idade} anos`
+
+        var genero = ''
+        
+        if(fsex[0].checked){
+            genero = 'Homem'
+        }  else if(fsex[1].checked){
+            genero='Mulher'
+        } 
+        res.innerHTML = `Detectamos que você é ${genero} com ${idade} anos`
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
+        if(genero == 'Mulher'){
+                         
+            if(idade<=10){
+                //crianca
+
+            }else if(idade<=18){
+                //adolescente
+
+            } else if(idade <=60){
+                //adulto 
+
+            }else{
+                //idoso
+            }
+        } else if(genero == 'Homem'){
+
+            if(idade<=10){
+                //crianca
+                img.setAttribute('src', 'crianaH.png')
+
+            }else if(idade<=18){
+                //adolescente
+
+            } else if(idade <=60){
+                //adulto 
+
+            }else{
+                //idoso
+            }
+        }
+        
+        
+        
+       
+    }
+}    
